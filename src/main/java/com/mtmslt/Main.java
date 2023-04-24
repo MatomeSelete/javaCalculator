632a39127bd8ca7680b2a77c38b0d53a69b8df25
Matome <seletematome3@gmail.com>
2023-04-24 13:56:35 +0200

    greetresponse is working

--- src/main/java/com/mtmslt/Main.java
+++ src/main/java/com/mtmslt/Main.java
@@ -1,21 +1,23 @@
 package com.mtmslt;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
 @RestController
 public class Main {
     public static void main(String[] args) {
-
         SpringApplication.run(Main.class, args);
     }
-
-    @GetMapping( "/")
-    public String greet() {
-        return "Hello";
+    @GetMapping( "/greet")
+    public GreetResponse greet() {
+        return new GreetResponse("Hello world");
     }
+    record GreetResponse(String greet) {}
 
 }
