68f727fee71530ab656bce414a8c2bca5768a89a
Matome <seletematome3@gmail.com>
2023-05-17 16:28:55 +0200

    connected to the databse but the project is not running

--- src/main/java/com/mtmslt/Main.java
+++ src/main/java/com/mtmslt/Main.java
@@ -8,35 +8,34 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @SpringBootApplication
-
 @RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-
     }
-
-    @GetMapping("/greet")
-    public GreetResponse greet() {
-         GreetResponse respons = new GreetResponse(
-                "Hello Matome ",
-                List.of("Java", "Python", "JavaScript"),
-                new Person("Matome", 25, 100_000)
-    );
-        return respons;
-    }
-
-    record Person(String name, int age, double savings) {
-
-    }
-
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person
-    ) {}
-
 
 }
 
 // going for SpringBoot tutorial coz wow
+
+
+//    @GetMapping("/greet")
+//    public GreetResponse greet() {
+//        GreetResponse respons = new GreetResponse(
+//                "Hello Matome ",
+//                List.of("Java", "Python", "JavaScript"),
+//                new Person("Matome", 25, 100_000)
+//        );
+//        return respons;
+//    }
+//
+//record Person(String name, int age, double savings) {
+//
+//}
+//
+//record GreetResponse(
+//        String greet,
+//        List<String> favProgrammingLanguages,
+//        Person person
+//) {}
+//
