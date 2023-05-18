4f796b080da981bc96bf243b32f8a02f06409f19
Matome <seletematome3@gmail.com>
2023-05-18 16:09:30 +0200

    trying to fix this err Failed to configure a DataSource: no embedded datasource could be configured.

--- src/main/java/com/mtmslt/Main.java
+++ src/main/java/com/mtmslt/Main.java
@@ -2,40 +2,18 @@ package com.mtmslt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+//import java.util.List;
 
 @SpringBootApplication
 @RestController
+@Component
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
 }
-
-// going for SpringBoot tutorial coz wow
-
-
-//    @GetMapping("/greet")
-//    public GreetResponse greet() {
-//        GreetResponse respons = new GreetResponse(
-//                "Hello Matome ",
-//                List.of("Java", "Python", "JavaScript"),
-//                new Person("Matome", 25, 100_000)
-//        );
-//        return respons;
-//    }
-//
-//record Person(String name, int age, double savings) {
-//
-//}
-//
-//record GreetResponse(
-//        String greet,
-//        List<String> favProgrammingLanguages,
-//        Person person
-//) {}
-//
